9867bea7aeb1db41ff1ce16cb660d103151466eb
Dmitry Promsky <1293095+dmski@users.noreply.github.com>
2019-11-04 15:13:51 +0300

    Remove more unused debug code

--- main.m
+++ main.m
@@ -13,24 +13,4 @@ int main(int argc, char *argv[])
 	srandom(time(NULL));
 
     return NSApplicationMain(argc,  (const char **) argv);
-
-//    Semaphore* sem = [[Semaphore alloc] init];
-//    NSURL* url = [NSURL fileURLWithPath:@"/Users/dima/Music/Mzk/443080_vibe_w2gh.mp3"];
-//    DummyPlayer* player = [[DummyPlayer alloc] init];
-//
-//    Input* input = [[Input alloc] init];
-//    [input startWithUrl:url player:player];
-//
-//    while (![input ready]) {
-//        NSLog(@"Not ready");
-//    }
-//
-//    Output* output = [[Output alloc] init];
-//    [output setupWithPlayer:player];
-//    [output start];
-//    [output setVolume:100];
-//
-//    [sem waitIndefinitely];
-//
-//    return 0;
 }
